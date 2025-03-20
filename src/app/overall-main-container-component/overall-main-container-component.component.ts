a31fe637adff2fbc859a8b88b826b7a0936d0bae
TSIORISOA <maminjaramichel@yahoo.fr>
2025-03-20 12:35:50 +0100

    Adding Header, Footer & OverallMainContainer Components to / route

---
+++ src/app/overall-main-container-component/overall-main-container-component.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-overall-main-container-component',
+  imports: [],
+  templateUrl: './overall-main-container-component.component.html',
+  styleUrl: './overall-main-container-component.component.scss'
+})
+export class OverallMainContainerComponentComponent {
+
+}
